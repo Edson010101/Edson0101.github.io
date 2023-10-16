f6a9b865dd97e28e929b2d00eece297cacea4c0e
Edson0101 <my.edsonsousa@gmail.com>
2023-10-16 16:58:24 -0300

    inicializandoum novo projeto

---
+++ python/javas/teste.js
@@ -0,0 +1,8 @@
+function cliqueBtn() {
+  document.getElementById("novaCor").style.background = "#ED028D";
+  document.getElementById("novaCor").style.color = "back"
+}
+function cliqueBtn() {
+  document.getElementById("nova").style.background = "#ED028D";
+  document.getElementById("nova").style.color = "back"
+}
